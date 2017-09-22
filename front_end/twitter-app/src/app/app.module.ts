c0b6d43a8f93be08636b303dd8e340b64e18d18a
JasonHo <hexing_h@hotmail.com>
2017-09-22 00:04:41 -0400

    Add some component for front-end

--- front_end/twitter-app/src/app/app.module.ts
+++ front_end/twitter-app/src/app/app.module.ts
@@ -9,22 +9,35 @@ import { DataService } from './services/data.service';
 
 import { rooting } from './app.router';
 
-import { MdToolbarModule, MdInputModule } from '@angular/material';
+import { MdToolbarModule, MdInputModule, MdMenuModule, 
+         MdIconModule, MdButtonModule, MdCardModule } from '@angular/material';
 import { UserInfoComponent } from './components/user-info/user-info.component';
+import { TweetlistComponent } from './components/tweetlist/tweetlist.component';
+import { PostareaComponent } from './components/postarea/postarea.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    UserInfoComponent
+    UserInfoComponent,
+    TweetlistComponent,
+    PostareaComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdToolbarModule,
     MdInputModule,
+    MdMenuModule,
+    MdIconModule,
+    MdButtonModule,
+    MdCardModule,
     rooting
   ],
-  providers: [{provide: "data",
+  providers: [{provide: 'data',
               useClass: DataService}],
   bootstrap: [AppComponent]
 })
--- front_end/twitter-app/src/app/app.router.ts
+++ front_end/twitter-app/src/app/app.router.ts
@@ -1,7 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 import { UserInfoComponent } from './components/user-info/user-info.component';
+import { TweetlistComponent } from './components/tweetlist/tweetlist.component';
 const appRoutes: Routes = [
     { path: 'test', component: UserInfoComponent },
+    { path: 'test/list', component: TweetlistComponent},
     { path: '**', redirectTo: 'test' }
 ];
 export const rooting = RouterModule.forRoot(appRoutes);
