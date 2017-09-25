f7b7f7a97caf66440b793ffa83f0d60a020f20dc
c78867886@hotmail.com <c78867886@hotmail.com>
2017-09-25 14:35:08 -0400

    Add func of fetching UserInfo and its testing

--- back_end/handler/user.go
+++ back_end/handler/user.go
@@ -1,12 +1,10 @@
 package handler
 
 import (
-	//"net/http"
-	//"github.com/labstack/echo"
-	//"github.com/dgrijalva/jwt-go"
-	//"gopkg.in/mgo.v2"
-	//"gopkg.in/mgo.v2/bson"
-	//"model"
+	"net/http"
+	"gopkg.in/mgo.v2/bson"
+	"github.com/labstack/echo"
+	"model"
 )
 
 /*func userIDFromToken(c echo.Context) string {
@@ -14,3 +12,35 @@ import (
 	claims := user.Claims.(jwt.MapClaims)
 	return claims["id"].(string)
 }*/
+type userContainer struct {
+	FirstName 	string			`json:"firstname"`
+	LastName	string			`json:"lastname"`
+	Password 	string			`json:"password"`
+	Email 		string			`json:"email"`
+}
+
+func (h *Handler) FetchUserInfo (c echo.Context) (err error) {
+	userID := c.Param("user")
+
+	// Retrieve user infomation from database
+	userInfo := []*model.User{}
+	db := h.DB.Clone()
+
+	err = db.DB("se_avangers").C("users").Find(bson.M{"useriddev": userID}).All(&userInfo)
+	if err != nil {
+		return
+	}
+	defer db.Close()
+	
+	res := []userContainer{}
+	for _, u := range userInfo {
+		firstname := u.FirstName
+		lastname := u.LastName
+		password := u.Password
+		email := u.Email
+		res = append(res, userContainer{
+			FirstName:firstname, LastName:lastname, Password:password , Email:email })
+	}
+	
+	return c.JSON(http.StatusOK, res)
+}
---
+++ back_end/handler/user_test.go
@@ -0,0 +1,55 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"gopkg.in/mgo.v2"
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFetchUserInfo (t *testing.T) {
+	session, err := mgo.Dial("mongodb://SEavanger:SEavanger@ds139964.mlab.com:39964/se_avangers")
+	if err != nil {
+		panic(err)
+	}
+
+	// Setup
+	e := echo.New()
+	h := &Handler{session}
+
+	// test cases
+	requestParam := []string {
+		"JasonHo", 
+		"MarsLee",
+		"JasonHe",  
+		"DianeLin"}
+
+	expectedJSON := []string {
+		`[{"firstname":"Jason","lastname":"Ho","password":"test1","email":"hojason117@gmail.com"}]`, 
+		`[{"firstname":"Chih-Yin","lastname":"Lee","password":"test2","email":"c788678867886@gmail.com"}]`, 
+		`[{"firstname":"Jason","lastname":"He","password":"test3","email":"hexing_h@hotmail.com"}]`, 
+		`[{"firstname":"Diane","lastname":"Lin","password":"test4","email":"diane@gmail.com"}]`,
+	}
+
+	// Run
+	for i, rp := range requestParam {
+		// Setup
+		req := httptest.NewRequest(echo.GET, "/", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		// Set the registered path for the handler.
+		c.SetPath("/api/v1/tweetlist")
+		// Set path parameter names.
+		c.SetParamNames("user")
+		// Set path parameter values.
+		c.SetParamValues(rp)
+
+		// Assertion
+		if assert.NoError(t, h.FetchUserInfo(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.Equal(t, expectedJSON[i], rec.Body.String())
+		}
+	}
+}
