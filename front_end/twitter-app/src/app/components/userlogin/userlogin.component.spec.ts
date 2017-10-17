419a8193de29cea841632627f1573df25aa3033a
Diane <tbaby.menger@gmail.com>
2017-10-17 14:29:06 -0400

    Update userlogin.component.spec.ts

--- front_end/twitter-app/src/app/components/userlogin/userlogin.component.spec.ts
+++ front_end/twitter-app/src/app/components/userlogin/userlogin.component.spec.ts
@@ -1,70 +1,60 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
-import { SignUpComponent } from './sign-up.component';
+import { NgModule } from '@angular/core';
+import { UserloginComponent } from './userlogin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { rooting } from '../../app.router';
 import  'rxjs/add/Observable/from';
 import  'rxjs/add/Observable/empty';
 import  'rxjs/add/Observable/throw';
 
-describe('SignUpComponent', () => {
-  let component: SignUpComponent;
-  // let router: Router;
+describe('UserloginComponent', () => {
+  let component: UserloginComponent;
+  let fixture: ComponentFixture<UserloginComponent>;
   let authService: AuthService;
-  let fixture: ComponentFixture<SignUpComponent>;
-  let auth;
+  let _route: Router;
 
   beforeEach(async(() => {
-    // TestBed.configureTestingModule({
-    //   imports: [ ReactiveFormsModule],
-    //   declarations: [ SignUpComponent ],
-    //   providers: [AuthService],
-    // })
-    // .compileComponents();
+
   }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ ReactiveFormsModule],
-      declarations: [ SignUpComponent ],
-      providers: [AuthService],
-    })
-    .compileComponents();
     
-    // auth = TestBed.get(AuthService);
-    // fixture = TestBed.createComponent(SignUpComponent);
-    authService = new AuthService(null);
-    component = new SignUpComponent(authService, null);
-
+    TestBed.configureTestingModule({
+        imports: [ ReactiveFormsModule, FormsModule ],
+        declarations: [ UserloginComponent ],
+        providers: [AuthService ],
+      })
+      .compileComponents();
+      // fixture = 
+      authService = new AuthService(null);
+      component = new UserloginComponent(authService, _route);
   });
 
-  // it ('should be sign up in successful if signup() successfully', () => {
-  //   let userInputInfo = "username";
-  //   let backAns = {"username": "a", "userid": 1}; 
-  //   spyOn(authService, 'signUp').and.callFake(() => {
+  // it ('should be login successful if login() successfully', () => {
+  //   let backAns = {"username": "a", "id": "1"}; 
+  //   spyOn(authService, 'login').and.callFake(() => {
   //     return Observable.from([backAns]);
   //   })
      
-  //   component.signUp(userInputInfo);
+  //   component.login();
 
-  //   expect(component.successMessage).toContain("Thank you");
+  //   expect(component.successMessage).toContain("Login success");
   // })
 
   it ('should be signup failure if signup() is failed', () => {
-    let userInputInfo = "username";
-    let backAns = {"username": "a", "userid": 1}; 
-    spyOn(authService, 'signUp').and.callFake(() => {
+    spyOn(authService, 'login').and.callFake(() => {
       return Observable.throw(Error);
     })
      
-    component.signUp(userInputInfo);
+    component.login();
 
     expect(component.successMessage).toBeNull;
-    expect(component.errorMessage).toContain("Something is wrong");
+    expect(component.errorMessage).toContain("combination is not in our records");
   })
-
-  
 });
+
