6842a69dbc91b6ed32cb3e51067f80d8bd739497
Diane Lin <[tbaby.menger@gmail.com]>
2017-11-05 15:11:14 -0500

    sd

---
+++ front_end/twitter-app/src/app/components/edit-comments-dialog/edit-comments-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-comments-dialog',
+  templateUrl: './edit-comments-dialog.component.html',
+  styleUrls: ['./edit-comments-dialog.component.css']
+})
+export class EditCommentsDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
